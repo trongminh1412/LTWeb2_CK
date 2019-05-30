c7194a7d38ccb92e7e9c2c8f0907c6c3c8abb70e
trongminh1412 <43989815+trongminh1412@users.noreply.github.com>
2019-05-30 22:13:07 +0700

    Revert "New Commit"
    This reverts commit 5b098a4d5ae60d45a02c0ef6820360134bac8b48.

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname'); // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-
