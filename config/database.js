5b098a4d5ae60d45a02c0ef6820360134bac8b48
trongminh1412 <43989815+trongminh1412@users.noreply.github.com>
2019-05-30 07:44:20 +0700

    New Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname'); // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+
